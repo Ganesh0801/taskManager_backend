aa3c12bdc2c75bc6c6c0f3245aace5f8b7db570a
Ganesh0801 <ganeshsbsa08@gmail.com>
2026-06-29 15:50:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const taskRoutes = require("./routes/task.routes");
+
+const app = express();
+const PORT = 8082;
+const DB_URI = "mongodb://localhost:27017/task-manager";
+
+mongoose
+  .connect(DB_URI)
+  .then(() => console.log("DB Connected!"))
+  .catch((error) => console.log("Error in connecting DB", error));
+
+app.use(cors());
+app.use(express.json());
+app.use("/tasks", taskRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Backend listening on Port ${PORT}!`);
+});
